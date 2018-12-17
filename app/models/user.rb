1d083c6914c38d169fbc2089b1648aba8ef0fdea
Joseph Davis <joseph.davis@rtslabs.com>
2018-12-17 21:22:44 +0000

    Update user view return

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ApplicationRecord
   has_many :skills, :through => :user_skills
 
   def as_json(options={})
-    super(only: [:id, :username, :first_name, :last_name, :email_address, :job_title])
+    super(only: [:id, :username, :first_name, :last_name, :email_address, :job_title],
+          include: [ skills: {
+                      only: [:id, :skill_name],
+                      include: [ categories: {
+                          only: [:id, :category_name]
+                        }]
+                    } ])
   end
 end
