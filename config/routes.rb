10bc7b714de34e6ee7cf57879b324984baa432e1
Joseph Davis <joseph.davis@rtslabs.com>
2018-12-17 16:58:06 -0500

    Add homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root "welcome#index"
   resources :categories, only: [ :index, :show ]
   resources :users do
 
@@ -8,5 +9,6 @@ Rails.application.routes.draw do
   resources :search, only: [ :index ]
 
   post 'login', to: 'users#login'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
